0fcb29c525215260db2584e3b4b4ddfa95153dec
Rushclin Takam <takamrushclin@gmail.com>
2025-02-27 11:10:01 +0100

    Remove comment on next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,5 @@
 import type { NextConfig } from "next";
 
-// cron.ts
-import cron from "node-cron";
-
-// Planifiez une tâche pour s'exécuter toutes les minutes
-cron.schedule("* * * * *", () => {
-  // console.log("Cron job exécuté à:", new Date().toISOString());
-  // Ajoutez ici la logique de votre tâche
-});
-
-console.log("Cron job configuré");
-
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
