a8af77737c329d917f069c67f0f8de7343979449
Rushclin Takam <takamrushclin@gmail.com>
2025-03-07 11:58:46 +0100

    Ajout des indication pour configurer

--- src/app/pages/config/page.tsx
+++ src/app/pages/config/page.tsx
@@ -42,13 +42,6 @@ const ConfigPage = () => {
   const onSubmit = async (data: ConfigurationDto) => {
     try {
       setIsLoading(true)
-      // const response = await fetch("/api/company/configuration", {
-      //   method: "POST",
-      //   headers: {
-      //     "Content-Type": "application/json",
-      //   },
-      //   body: JSON.stringify({ ...data }),
-      // });
       const response = await saveConfiguration(data)
 
       if (!response) {
@@ -77,67 +70,97 @@ const ConfigPage = () => {
   }
 
   return (
-    <div className='border border-dashed w-full max-w-lg mx-auto rounded-md p-4 bg-white shadow-md'>
+    <div className='border border-dashed w-full max-w-5xl mx-auto rounded-md p-4 bg-white shadow-md'>
       <h2 className='text-xl font-semibold text-gray-700 mb-4'>Configuration</h2>
 
-      <form onSubmit={handleSubmit(onSubmit)} className='space-y-4'>
-        <div>
-          <label className='block text-sm font-medium text-gray-600'>API URL</label>
-          <Input
-            className='w-full border rounded-md p-2'
-            type='url'
-            {...register('apiUrl', { required: 'L’URL de l’API est requise.', pattern: { value: /^(https?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-._~:/?#[\]@!$&'()*+,;=.]+$/, message: 'URL invalide' } })}
-          />
-          {errors.apiUrl && <p className='text-red-500 text-sm'>{errors.apiUrl.message?.toString()}</p>}
+      <div className='flex gap-4'>
+        <div className='w-1/2'>
+          <p className='text-gray-600 text-md font-light'>
+            Pour éffectuer la configuration complète, veuillez suivre ces étapes.
+          </p>
+          <h4 className='mt-3 font-bold'>Woo Commerce</h4>
+          <div className='px-5 mt-2 text-md font-light'>
+            <ul className='list-decimal'>
+              <li>Aller dans votre boutique <span className='font-bold'>Woo Commerce</span></li>
+              <li>Puis choisir l&apos;option <span className='font-bold'>Réglages</span></li>
+              <li>Ensuite, choisir l&apos;option <span className='font-bold'>Avancé</span></li>
+              <li>Enfin, choisir l&apos;option <span className='font-bold'>API REST</span></li>
+              <li>Cliquer sur l&apos;option ajouter une nouvelle clef </li>
+              <li>Remplir les champs adepquetes et choisir le droit de <span className='font-bold'>Lecture/Ecriture</span> </li>
+            </ul>
+          </div>
+
+          <h4 className='mt-3 font-bold'>Genuka</h4>
+          <div className='px-5 mt-2 text-md font-light'>
+            <ul className='list-decimal'>
+              <li>Aller dans votre boutique <span className='font-bold'>Genuka</span></li>
+              <li>Ensuite dans <span className='font-bold'>Parametres</span> </li>
+              <li>Choisir l&apos;option clef <span className='font-bold'>API et remplir</span> </li>
+            </ul>
+          </div>
+
         </div>
 
-        <div>
-          <label className='block text-sm font-medium text-gray-600'>Consumer Key</label>
-          <Input
-            className='w-full border rounded-md p-2'
-            type='text'
-            {...register('consumerKey', { required: 'La clé consommateur est requise.' })}
-          />
-          {errors.consumerKey && <p className='text-red-500 text-sm'>{errors.consumerKey.message?.toString()}</p>}
-        </div>
+        <div className='w-1/2'>
+          <form onSubmit={handleSubmit(onSubmit)} className='space-y-4'>
+            <div>
+              <label className='block text-sm font-medium text-gray-600'>API URL</label>
+              <Input
+                className='w-full border rounded-md p-2'
+                type='url'
+                {...register('apiUrl', { required: 'L’URL de l’API est requise.', pattern: { value: /^(https?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-._~:/?#[\]@!$&'()*+,;=.]+$/, message: 'URL invalide' } })}
+              />
+              {errors.apiUrl && <p className='text-red-500 text-sm'>{errors.apiUrl.message?.toString()}</p>}
+            </div>
 
-        <div>
-          <label className='block text-sm font-medium text-gray-600'>Consumer Secret</label>
-          <Input
-            className='w-full border rounded-md p-2'
-            type='password'
-            {...register('consumerSecret', { required: 'Le secret consommateur est requis.' })}
-          />
-          {errors.consumerSecret && <p className='text-red-500 text-sm'>{errors.consumerSecret.message?.toString()}</p>}
-        </div>
+            <div>
+              <label className='block text-sm font-medium text-gray-600'>Consumer Key</label>
+              <Input
+                className='w-full border rounded-md p-2'
+                type='text'
+                {...register('consumerKey', { required: 'La clé consommateur est requise.' })}
+              />
+              {errors.consumerKey && <p className='text-red-500 text-sm'>{errors.consumerKey.message?.toString()}</p>}
+            </div>
 
-        <div>
-          <label className='block text-sm font-medium text-gray-600'>App Version</label>
-          <Input
-            className='w-full border rounded-md p-2 bg-gray-100'
-            type='text'
-            defaultValue='wc/v3'
-            {...register('appVersion', { required: 'La version de l’application est requise.' })}
-            readOnly
-          />
-        </div>
+            <div>
+              <label className='block text-sm font-medium text-gray-600'>Consumer Secret</label>
+              <Input
+                className='w-full border rounded-md p-2'
+                type='password'
+                {...register('consumerSecret', { required: 'Le secret consommateur est requis.' })}
+              />
+              {errors.consumerSecret && <p className='text-red-500 text-sm'>{errors.consumerSecret.message?.toString()}</p>}
+            </div>
 
-        <div className='mt-5'>
-          <label className='block text-sm font-medium text-gray-600'>Cle d&apos;API</label>
-          <Input
-            className='w-full border rounded-md p-2'
-            type='password'
-            {...register('apiKey', { required: 'La clé d\'API est requise.' })}
-          />
-          {errors.apiKey && <p className='text-red-500 text-sm'>{errors.apiKey.message?.toString()}</p>}
-        </div>
+            <div>
+              <label className='block text-sm font-medium text-gray-600'>App Version</label>
+              <Input
+                className='w-full border rounded-md p-2 bg-gray-100'
+                type='text'
+                defaultValue='wc/v3'
+                {...register('appVersion', { required: 'La version de l’application est requise.' })}
+                readOnly
+              />
+            </div>
 
-        <Button type='submit' className='w-full bg-[#873EFF] text-white p-2 rounded-md'>
-          Sauvegarder
-        </Button>
-      </form>
-    </div>
-  );
+            <div className='mt-5'>
+              <label className='block text-sm font-medium text-gray-600'>Cle d&apos;API</label>
+              <Input
+                className='w-full border rounded-md p-2'
+                type='password'
+                {...register('apiKey', { required: 'La clé d\'API est requise.' })}
+              />
+              {errors.apiKey && <p className='text-red-500 text-sm'>{errors.apiKey.message?.toString()}</p>}
+            </div>
+
+            <Button type='submit' className='w-full bg-[#873EFF] text-white p-2 rounded-md'>
+              Sauvegarder
+            </Button>
+          </form>
+        </div>
+      </div>
+    </div>);
 };
 
 export default ConfigPage;
